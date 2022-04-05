fd36665ae73964844169a647154dfc7d2265da83
ensav <ensav@LAPTOP-R8QF36DG>
2022-04-05 15:26:42 -0400

    Add project files.

---
+++ .svn/pristine/c5/c5f24f2571157a07292aac130bec7231774e5820.svn-base
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    // controls all things dealing with the locations (rooms)
+    // might need to look into changing the way the rooms work... originally used just an XY Coordinate system, I am going to use XYZ
+    // using the Z axis allows for there to be towers and such
+    // Z axis is for the vertical axis 
+    // X axis and Y axis make up the 2d plane at the base of the world
+    // this way the buildings can be as tall as I would like to make them
+    public class Location
+    {
+        public int XCoordinate {  get; set; }
+        public int YCoordinate { get; set; }
+        public int ZCoordinate { get; set; }    
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+    }
+}
---
+++ Engine/Models/Player.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * new project to keep the models in
+ * 
+ * Model is used to describe classes 
+ * which represent real world objects
+ * such as the player
+ */
+namespace Engine.Models
+{
+    public class Player : INotifyPropertyChanged
+    {
+        //  backing value for the experience points field so event
+        //  can change the value
+        // backing variable is a variable the property uses to store its value
+        private string _name;
+        private string _characterClass;
+        private int _hitPoints;
+        private int _expereincePoints;
+        private int _level;
+        private int _gold;
+
+
+       public  string Name
+        { 
+            get { return _name; }
+            set 
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            } 
+        }
+       public string CharacterClass 
+        { 
+            get { return _characterClass; }
+            set
+            {
+                _characterClass = value;
+                OnPropertyChanged("CharacterClass");
+            } 
+        }
+       public int HitPoints
+        {
+            get { return _hitPoints;}
+            set
+            {
+                _hitPoints = value;
+                OnPropertyChanged("HitPoints");
+            }
+        }
+       public int ExperiencePoints
+        {
+            get { return _expereincePoints; }
+            set 
+            {
+                _expereincePoints = value;
+                OnPropertyChanged("ExperiencePoints");
+            }
+        }
+       public int Level 
+        {
+            get { return _level;}
+            set
+            {
+                _level = value;
+                OnPropertyChanged("Level");
+            }
+        }
+       public  int Gold 
+        {
+            get { return _gold;}
+            set
+            {
+                _gold = value;
+                OnPropertyChanged("Gold");
+            }
+        }
+
+        // event to change the properties
+       public event PropertyChangedEventHandler? PropertyChanged;
+
+       protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
